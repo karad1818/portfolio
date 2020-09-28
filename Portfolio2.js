c1e7984fa3ea1ee293120cac01ba4a691822dc1c
karad1818 <52450149+karad1818@users.noreply.github.com>
2020-09-28 21:47:16 +0530

    Update Portfolio2.js

--- Portfolio2.js
+++ Portfolio2.js
@@ -8,23 +8,23 @@ toggle.addEventListener("click",function(){
 /*   home card */
 let my_img = document.querySelector(".my_img");
 my_img.addEventListener("mouseout",function(){
-	my_img.style.backgroundImage='url("Images/My1.jpg")';
+	my_img.style.backgroundImage="url('Images/My1.jpg')";
 	my_img.style.cursor="auto";
 });
 let link = document.querySelector(".c1");
 link.addEventListener("mouseover",function(){
-	my_img.style.backgroundImage='url("Images/link.PNG")';
+	my_img.style.backgroundImage="url('Images/link.PNG')";
 	my_img.style.cursor="pointer";
 });
 let link2 = document.querySelector(".c2");
 link2.addEventListener("mouseover",function(){
-	my_img.style.backgroundImage='url("Images/link2.PNG")';
+	my_img.style.backgroundImage="url('Images/link2.PNG')";
 	my_img.style.cursor="pointer";
 
 });
 let link3 = document.querySelector(".c3");
 link3.addEventListener("mouseover",function(){
-	my_img.style.backgroundImage='url("Images/link3.PNG")';
+	my_img.style.backgroundImage="url('Images/link3.PNG')";
 	my_img.style.cursor="pointer";
 });
 
